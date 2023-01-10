31019f5fdb96cb1822341c73bdc8e5bb10b243a8
bhaia9036@gmail.com <alphaahmed746@gmail.com>
2023-01-11 02:16:44 +0500

    Last Commit

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -48,9 +48,12 @@ const LogInPage = () => {
 					</div>
 					
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+					<Link to={'/SignupForm3'} >
+					<button class="login100-form-btn">
 							Signup
 						</button>
+						</Link>
+						
 					</div>
 
 				
